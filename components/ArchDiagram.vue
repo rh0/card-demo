80be779eeaa5e3f7dd4a742a357236083fcc15bd
rho <ryan@theoleschool.com>
2021-02-09 20:24:26 -0600

    svg and component file

---
+++ components/ArchDiagram.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-card
+    flat
+    tile
+    color="transparent"
+  >
+  <v-card-title>Top Level Diagram of WTF is going on.</v-card-title>
+    <v-card-text>
+      <img src="/card-demo/component-diagram-dark.svg"/>
+    </v-card-text>
+  </v-card>
+</template>
