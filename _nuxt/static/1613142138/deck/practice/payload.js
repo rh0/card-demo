68d33c21fad0fe50a1bfea1b6e42363aa17c58b5
rho <ryan@theoleschool.com>
2021-02-12 09:09:33 -0600

    6ebb399

---
+++ _nuxt/static/1613142138/deck/practice/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/deck/practice", {data:[{}],fetch:[],mutations:[]});
---
+++ _nuxt/static/1613142138/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fdeck\u002Fpractice","\u002Fdecks","\u002Farchitecture"]})
